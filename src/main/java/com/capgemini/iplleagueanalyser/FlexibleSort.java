013f2b2b1a7bf8b879c87e0d034f67ff11cc7cff
Your Name <you@example.com>
2020-10-28 20:18:28 +0530

    [shashi] Refactored : Separate method to calculate value

--- src/main/java/com/capgemini/iplleagueanalyser/FlexibleSort.java
+++ src/main/java/com/capgemini/iplleagueanalyser/FlexibleSort.java
@@ -34,10 +34,8 @@ public class FlexibleSort implements Comparator<Batting> {
 			if (value == 0) {
 				return (Integer.parseInt(b2.getFours()) + Integer.parseInt(b2.getSixes()))
 						- (Integer.parseInt(b1.getFours()) + Integer.parseInt(b1.getSixes()));
-			} else if (value > 0)
-				value = 1;
-			else if (value < 0)
-				value = -1;
+			}
+			value = setValue(value);
 			return (int) value;
 		case AVG_AND_SR:
 			if (b1.getAvg().contains("-"))
@@ -45,14 +43,20 @@ public class FlexibleSort implements Comparator<Batting> {
 			value = (Double.parseDouble(b2.getAvg()) - Double.parseDouble((b1.getAvg())));
 			if (value == 0) {
 				return (int) (Double.parseDouble(b2.getStrikeRate()) - Double.parseDouble((b1.getStrikeRate())));
-			} else if (value > 0)
-				value = 1;
-			else if (value < 0)
-				value = -1;
+			}
+			value = setValue(value);
 			return (int) value;
 		default:
 			break;
 		}
 		return 0;
+	}
+
+	public double setValue(double value) {
+		if (value > 0)
+			value = 1;
+		else if (value < 0)
+			value = -1;
+		return value;
 	}
 }
